c249072092eb4b7b0cf97fdc705417da1f932cc3
MLuiz1992 <matheus.oliveira429@etec.sp.gov.br>
2017-05-17 18:59:27 -0300

    Inserção e mostra de atores pronta, já com bootstrap, sistema anti-troll funcionando nos atores :)

--- resources/views/atores/create.blade.php
+++ resources/views/atores/create.blade.php
@@ -1,44 +1,36 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.app')
+@section('content')
+<?php
+        if (Auth::check()){
+            ?> <div class='container'>
+        <div class='row'>
 
-<head>
-    <title></title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
-        crossorigin="anonymous">
-</head>
+            <h1 class='page-header'>Inserção de Atores</h1>
 
-<body>
-
-    <div class="container">
-        <div class="row">
-
-            <h1 class="page-header">Inserção de Atores</h1>
-
-            <div class="col-md-6">
-                <form action="{{ route('atores.store')}}" method="post">
+            <div class='col-md-6'>
+                <form action='{{ route('atores.store')}}' method='post'>
 
                     {{csrf_field()}}
 
-                    <div class="form-group">
-                        <label for="nome">Nome</label>
-                        <input id="nome" class="form-control" type="text" name="nome" placeholder="Nome">
+                    <div class='form-group'>
+                        <label for='nome'>Nome</label>
+                        <input id='nome' class='form-control' type='text' name='nome' placeholder='Nome'>
 
                     </div>
-                    <div class="form-group">
-                        <label for="ano">Ano nascimento</label>
-                        <input id="ano" class="form-control" type="text" name="ano" placeholder="Ano">
+                    <div class='form-group'>
+                        <label for='ano'>Ano nascimento</label>
+                        <input id='ano' class='form-control' type='text' name='ano' placeholder='Ano'>
 
                     </div>
 
                    
-                    <button type="submit" class="btn btn-primary">Enviar</button>
+                    <button type='submit' class='btn btn-primary'>Enviar</button>
                 </form>
             </div>
         </div>
-    </div>
-
-</body>
-
-</html>
+    </div> <?php
+        }else{
+            echo "<h1 class='text-center'>Faça o login para poder cadastrar os atores</h1>";
+        }
+        ?>
+        @endsection
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,20 +1,16 @@
 @extends('layouts.app')
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <title></title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">    </head>
-    <body>
         @section('content')
         <?php 
         if (Auth::check()){
-            echo "<h1 class='text-center'>Palmeiras não tem Mundial :D</h1>";
-        }else{
-            echo "<h1 class='text-center'>Faça o login para ver o Easter Egg!</h1>";
+            ?><div class="container">
+            <h1 class='text-center'>Escolha dentre as opções abaixo:</h1>
+            <p><a class="btn btn-primary col-md-12" href="/atores">Atores</a></p>
+            <p><a class="btn btn-warning col-md-12" href="/generos">Gêneros</a></p>
+            <p><a class="btn btn-danger col-md-12" href="/filmes">Filmes</a></p>
+            </div>
+            <?php
+            }else{
+            echo "<h1 class='text-center'>Faça o login para ter acesso ao sistema</h1>";
         }
         ?>
         @endsection
-    </body>
-</html>
